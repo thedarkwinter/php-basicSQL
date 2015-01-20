3e3f507bcf7d132b3434e609e30ef9de51bfb5bd
Michael Holloway <michael@thedarkwinter.com>
2015-01-20 18:14:32 +0100

    converted to namespace / dynamic class

--- basicSQL.php
+++ basicSQL.php
@@ -10,26 +10,29 @@
  * See README.md for usage
  */
 
+namespace thedarkwinter;
+use mysqli;
+
 class basicSQL
 {
-    private static $mysqli;
-    private static $database;
-    private static $host;
-    private static $port;
-    private static $user;
-    private static $password;
+    private $mysqli;
+    private $database;
+    private $host;
+    private $port;
+    private $user;
+    private $password;
 
-    private static $test_mode;
-    private static $error_log_length = 100;
-    private static $query_log_length = 100;
-    private static $error_log = array();
-    private static $query_log  = array();
-    private static $totals = array('exec_time' => 0, 'errors' => 0, 'warnings' => 0, 'slow_queries' => 0);
+    private $test_mode;
+    private $error_log_length = 100;
+    private $query_log_length = 100;
+    private $error_log = array();
+    private $query_log  = array();
+    private $totals = array('exec_time' => 0, 'errors' => 0, 'warnings' => 0, 'slow_queries' => 0);
 
-    private static $error_callback = array();
-    private static $query_callback = array();
-    private static $slow_query_callback = array();
-    private static $slow_query_time = 5;
+    private $error_callback = array();
+    private $query_callback = array();
+    private $slow_query_callback = array();
+    private $slow_query_time = 5;
 
     /**
      * Connects to database
@@ -41,26 +44,26 @@ class basicSQL
     public function connect($params = array())
     {
         // return active connection if exists
-        if (isset(self::$con) && mysql_ping(self::$con)) {
-            return self::$con;
+        if (isset($this->con) && mysql_ping($this->con)) {
+            return $this->con;
         }
 
         // load db defaults from php.ini
         foreach (array('database', 'host', 'port', 'user', 'password') as $p) {
-            self::$$p = ini_get("mysql.default_{$p}");
+            $this->$p = ini_get("mysql.default_{$p}");
         }
         foreach ($params as $p => $v) {
-            if (isset(self::$$p)) {
-                self::$$p = $v;
+            if (isset($this->$p)) {
+                $this->$p = $v;
             }
         }
 
         if (isset($params['test_mode']) && $params['test_mode'] == true) {
-            self::$test_mode = true;
+            $this->test_mode = true;
         }
-        self::$mysqli = new mysqli(self::$host.((self::$port != '') ? ':'.self::$port : ''), self::$user, self::$password, self::$database);
-        if (self::$mysqli->connect_errno) {
-            self::addError('connect', '', self::$mysqli->connect_errno, self::$mysqli->connect_error);
+        $this->mysqli = new mysqli($this->host.(($this->port != '') ? ':'.$this->port : ''), $this->user, $this->password, $this->database);
+        if ($this->mysqli->connect_errno) {
+            self::addError('connect', '', $this->mysqli->connect_errno, $this->mysqli->connect_error);
 
             return false;
         }
@@ -75,8 +78,8 @@ class basicSQL
      */
     public function close()
     {
-        if (self::$mysqli) {
-            return self::$mysqli->close();
+        if ($this->mysqli) {
+            return $this->mysqli->close();
         }
 
         return true;
@@ -92,9 +95,9 @@ class basicSQL
      *
      * @return boolean
      */
-    private function addError($type, $query, $no, $error)
+    public function addError($type, $query, $no, $error)
     {
-        self::$totals['errors']++;
+        $this->totals['errors']++;
         $entry = array(
                 'time' => time(),
                 'type' => $type,
@@ -102,14 +105,14 @@ class basicSQL
                 'no' => $no,
                 'error' => $error,
             );
-        $index = count(self::$error_log);
-        self::$error_log[$index] = $entry;
+        $index = count($this->error_log);
+        $this->error_log[$index] = $entry;
 
-        if (is_callable(self::$error_callback)) {
-            call_user_func(self::$error_callback, $entry); // TODO: Add some protection here
+        if (is_callable($this->error_callback)) {
+            call_user_func($this->error_callback, $entry); // TODO: Add some protection here
         }
-        if (self::$error_log_length>0 && isset(self::$error_log[$index - self::$error_log_length])) {
-            unset(self::$error_log[$index - self::$error_log_length]);
+        if ($this->error_log_length>0 && isset($this->error_log[$index - $this->error_log_length])) {
+            unset($this->error_log[$index - $this->error_log_length]);
         }
 
         return true;
@@ -122,9 +125,9 @@ class basicSQL
      */
     public function lastError()
     {
-        $index = count(self::$error_log);
+        $index = count($this->error_log);
 
-        return array_values(self::$error_log[$index-1]);
+        return array_values($this->error_log[$index-1]);
     }
 
     /**
@@ -134,7 +137,7 @@ class basicSQL
      */
     public function errorLog()
     {
-        return array_values(self::$error_log);
+        return array_values($this->error_log);
     }
 
     /**
@@ -144,13 +147,13 @@ class basicSQL
      */
     private function addQueryLog($type, $query, $num_rows = 0, $field_count = 0, $insert_id = 0, $rows_affected = 0, $matched = 0, $changed = 0, $warnings = 0, $exec_time = 0, $error = false)
     {
-        if (!isset(self::$totals[$type.'s'])) {
-            self::$totals[$type.'s'] = 0;
+        if (!isset($this->totals[$type.'s'])) {
+            $this->totals[$type.'s'] = 0;
         }
-        self::$totals[$type.'s']++;
-        self::$totals['warnings'] += $warnings;
-        if ($exec_time > self::$slow_query_time) {
-            self::$totals['slow_queries']++;
+        $this->totals[$type.'s']++;
+        $this->totals['warnings'] += $warnings;
+        if ($exec_time > $this->slow_query_time) {
+            $this->totals['slow_queries']++;
         }
         $entry = array(
                 'time' => time(),
@@ -171,17 +174,17 @@ class basicSQL
                 unset($entry[$k]);
             }
         }
-        $index = count(self::$query_log);
-        self::$query_log[$index] = $entry;
+        $index = count($this->query_log);
+        $this->query_log[$index] = $entry;
 
-        if (is_callable(self::$query_callback)) {
-            call_user_func(self::$query_callback, $entry); // TODO: Add some protection here
+        if (is_callable($this->query_callback)) {
+            call_user_func($this->query_callback, $entry); // TODO: Add some protection here
         }
-        if ($exec_time > self::$slow_query_time && is_callable(self::$slow_query_callback)) {
-            call_user_func(self::$slow_query_callback, $entry); // TODO: Add some protection here
+        if ($exec_time > $this->slow_query_time && is_callable($this->slow_query_callback)) {
+            call_user_func($this->slow_query_callback, $entry); // TODO: Add some protection here
         }
-        if (self::$query_log_length>0 && isset(self::$query_log[$index-self::$query_log_length])) {
-            unset(self::$query_log[$index-self::$query_log_length]);
+        if ($this->query_log_length>0 && isset($this->query_log[$index-$this->query_log_length])) {
+            unset($this->query_log[$index-$this->query_log_length]);
         }
 
         return true;
@@ -194,7 +197,7 @@ class basicSQL
      */
     public function queryLog()
     {
-        return array_values(self::$query_log);
+        return array_values($this->query_log);
     }
 
     /**
@@ -205,7 +208,7 @@ class basicSQL
     public function slowQueryLog($exec_time = 1)
     {
         $slow_queriues = array();
-        foreach (array_values(self::$query_log) as $entry) {
+        foreach (array_values($this->query_log) as $entry) {
             if ($entry['exec_time']>$exec_time) {
                 $slow_queriues[] = $entry;
             }
@@ -221,7 +224,7 @@ class basicSQL
      */
     public function report()
     {
-        return self::$totals;
+        return $this->totals;
     }
 
     /**
@@ -231,34 +234,34 @@ class basicSQL
      *
      * @return int|mysql_resource Returns the index on an Insert statement, or mysql_resource
      */
-    public static function query($query)
+    public function query($query)
     {
         $type = strtolower(array_shift(explode(' ', trim($query))));
-        if (self::$test_mode && !in_array($type, array('SELECT', 'SHOW', 'DESC'))) {
+        if ($this->test_mode && !in_array($type, array('SELECT', 'SHOW', 'DESC'))) {
             self::addQueryLog($type, $query);
 
             return true;
         }
 
         $starttime = microtime();
-        $result = self::$mysqli->query($query);
+        $result = $this->mysqli->query($query);
         $exec_time = microtime() - $starttime;
-        self::$totals['exec_time'] += $exec_time;
+        $this->totals['exec_time'] += $exec_time;
 
         if (!$result) {
-            $error = self::addError($type, $query, self::$mysqli->errno, self::$mysqli->error);
+            $error = self::addError($type, $query, $this->mysqli->errno, $this->mysqli->error);
             self::addQueryLog($type, $query, 0, 0, 0, 0, 0, 0, 0, 0, $exec_time, $error);
 
             return false;
         }
 
-        $insert_id = ($type == 'insert') ? self::$mysqli->insert_id : 0;
+        $insert_id = ($type == 'insert') ? $this->mysqli->insert_id : 0;
         $num_rows = (isset($result->num_rows)) ? $result->num_rows : 0;
         $rows_affected = (isset($result->rows_affected)) ? $result->rows_affected : 0;
         $field_count = (isset($result->field_count)) ? $result->field_count : 0;
         $matched = $changed = $warnings = 0;
-        if (isset(self::$mysqli->info)) {
-            list($matched, $changed, $warnings) = sscanf(self::$mysqli->info, "Rows matched: %d Changed: %d Warnings: %d");
+        if (isset($this->mysqli->info)) {
+            list($matched, $changed, $warnings) = sscanf($this->mysqli->info, "Rows matched: %d Changed: %d Warnings: %d");
         }
         self::addQueryLog($type, $query, $num_rows, $field_count, $insert_id, $rows_affected, $matched, $changed, $warnings, $exec_time);
 
@@ -272,11 +275,11 @@ class basicSQL
      *
      * @return string|array Returns the single field or array of fields
      */
-    public static function lookup($field, $table, $where)
+    public function lookup($field, $table, $where)
     {
-        $field = self::$mysqli->real_escape_string($field);
-        $table = self::$mysqli->real_escape_string($table);
-        //$where = self::$mysqli->real_escape_string($where);
+        $field = $this->mysqli->real_escape_string($field);
+        $table = $this->mysqli->real_escape_string($table);
+        //$where = $this->mysqli->real_escape_string($where);
         $query = "SELECT $field FROM $table WHERE $where ORDER BY $field DESC LIMIT 0,1";
         $result = self::Query($query);
         if (!$result or $result->num_rows == 0) {
